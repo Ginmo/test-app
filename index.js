05614c53521ac8b409e5eb99b44295efd60f9259
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-27 14:06:08 +0200

    Api docs

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello");
-});
+const swaggerUi = require('swagger-ui-express');
+const openApiDocumentation = require('./ServiceManual.v1.json');
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
